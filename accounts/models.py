cd39f907baf885502b4baf12b4254033e82dc4c7
Reza Abedini <rezacrow88@gmail.com>
2024-07-20 23:12:34 +0330

    The user model was completed (image, user_type, phone were added)

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+import datetime
+
+def upload_to(instance, filename):
+    date = datetime.now().strftime('%Y%m%d')
+    filename = "myfiles" + "/" + date + "/" + filename
+    return filename
 
 class User(AbstractUser):
-    pass
+    USER_TYPE_CHOICES = (
+        ('V', 'ویژه'),
+        ('N', 'عادی'),
+    )
+
+    phone = models.CharField(max_length=11, blank=True, null=True)
+    image = models.ImageField(upload_to=upload_to, blank=True, null=True)
+    user_type = models.CharField(max_length=1, choices=USER_TYPE_CHOICES, default="N")
+
+    def __str__(self) -> str:
+        return f"{self.username}"
+    
+    class Meta:
+        verbose_name = 'کاربران'
+        verbose_name_plural = 'کاربران'
